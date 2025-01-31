ef83fd6d48457299a851fc9645f660710c2644b9
murleek <urla.maxim@gmail.com>
2025-01-31 14:52:00 +0200

    refactor: trying to optimize animations of theme buttons

--- src/components/ThemeSettings/index.tsx
+++ src/components/ThemeSettings/index.tsx
@@ -6,9 +6,9 @@ const ThemeSettings: FC = () => {
     localStorage.theme
   );
   const buttonClass =
-    "ease duration-200 w-full bg-white border-2 border-gray-100 text-black font-bold py-2 px-1 dark:bg-zinc-800 dark:border-zinc-700 dark:text-white first:rounded-l-xl last:rounded-r-xl  [&:not(:last-child)]:border-r-0";
+    "ease duration-200 w-full bg-white border-2 border-gray-100 text-black font-bold py-2 px-1 dark:bg-zinc-800 dark:border-zinc-700 dark:text-white first:rounded-l-xl last:rounded-r-xl [&:has(+.selected):not(:first-child)]:border-l-0 [&:has(+.selected)]:border-r-0 [&.selected+&:not(:last-child)]:border-r-0 [&.selected+&]:border-l-0";
   const selectedClasses =
-    "z-10 peer !border-r-2 [&+*]:border-l-0 !bg-blue-100 dark:!bg-blue-900 selected !border-blue-700 dark:!border-blue-500";
+    "peer !bg-blue-100 dark:!bg-blue-900 selected !border-blue-700 dark:!border-blue-500";
 
   useEffect(() => {
     if (colorScheme) {
