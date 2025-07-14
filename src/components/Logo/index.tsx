c2a04d53b8bf8786896ec6f11cfa7941332d94b8
Denys Urla <urladenys.dev.com>
2025-07-14 12:51:29 +0300

    chore: Adjusted watermark color according to the theme

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,13 @@
+import { useTheme } from "../../hooks/useTheme";
+
 const Logo = () => {
+  const { isMapInDarkMode } = useTheme();
   return (
     <div
       className="fixed z-10 w-dvw h-dvh bg-[url(/logo.svg)] flex items-center justify-center pointer-events-none bg-[size:auto_80px]"
       style={{
-        // backgroundSize: "auto max(32px, 5vmax)",
         backgroundPosition: "center center",
-        opacity: 0.08,
+        opacity: isMapInDarkMode ? 0.12 : 0.08,
       }}
     ></div>
   );
